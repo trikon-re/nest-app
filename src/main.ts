602d8591bff82fa8c060090905009ad181b7a4da
tam11a <ibrahimsadiktamim@gmail.com>
2023-08-17 02:55:44 +0600

    updated: swagger

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Hello Lead? - API')
+    .setTitle('trikon.re - api')
     .setDescription('Lead Management System + Sales Management CRM')
     .setVersion('1.0')
     .addBearerAuth()
