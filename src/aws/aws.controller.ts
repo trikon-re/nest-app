7a85dc074568942e90bcc6e8e340c5713ecf9851
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-21 18:55:29 +0600

    added: file upload limit

--- src/aws/aws.controller.ts
+++ src/aws/aws.controller.ts
@@ -1,5 +1,7 @@
 import {
   Controller,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -16,18 +18,26 @@ export class AwsController {
   constructor(private readonly awsService: AwsService) {}
 
   @Post('upload')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      limits: {
-        fileSize: 1024 * 1024 * 5,
-      },
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file'))
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     type: CreateAwDto,
   })
-  create(@UploadedFile() file: Express.Multer.File) {
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 50,
+            message() {
+              return `File size should not exceed 50mb!`;
+            },
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     if (file) return this.awsService.uploadFile(file);
     else throw new BadRequestException();
   }
